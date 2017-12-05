3c7b63ee644543a9bb5d7b1f69afa1345ff2e719
Ral Oliver <raloliver.ws@gmail.com>
2017-12-05 11:49:02 -0200

    add take picute page on modal and on event

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ModalController } from 'ionic-angular';
+
 
 import { PhotosPage } from './../photos/photos';
+import { TakePicturePage } from './../take-picture/take-picture';
 
 @Component({
   selector: 'page-home',
@@ -11,9 +13,15 @@ export class HomePage {
   public photosTab: any
   public profileTab: any
 
-  constructor(public navCtrl: NavController) {
+  constructor(private navCtrl: NavController, private modalCtrl: ModalController) {
     this.photosTab = PhotosPage
     this.profileTab = PhotosPage
+  }
+
+  showTakePicture() {
+    //choose the page will show on modal
+    let modal = this.modalCtrl.create(TakePicturePage)
+    modal.present()
   }
 
 }
