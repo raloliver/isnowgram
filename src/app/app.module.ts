6e1ef001031a98cfd31775127cf46de98ba4ce04
Ral Oliver <raloliver.ws@gmail.com>
2017-12-05 11:39:32 -0200

    add page to take picture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/login';
 import { SignupPage } from '../pages/signup/signup';
 import { PhotosPage } from './../pages/photos/photos';
+import { TakePicturePage } from './../pages/take-picture/take-picture';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { PhotosPage } from './../pages/photos/photos';
     HomePage,
     LoginPage,
     SignupPage,
-    PhotosPage
+    PhotosPage,
+    TakePicturePage
   ],
   imports: [
     BrowserModule,
@@ -28,12 +30,13 @@ import { PhotosPage } from './../pages/photos/photos';
     HomePage,
     LoginPage,
     SignupPage,
-    PhotosPage
+    PhotosPage,
+    TakePicturePage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/pages/take-picture/take-picture.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-take-picture',
+  templateUrl: 'take-picture.html'
+})
+export class TakePicturePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
