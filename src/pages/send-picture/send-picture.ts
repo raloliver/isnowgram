c673652cc11af9084309f4701618d0aa57ec763f
Ral Oliver <raloliver.ws@gmail.com>
2017-12-08 18:00:29 -0200

    geolocation

--- src/pages/send-picture/send-picture.ts
+++ src/pages/send-picture/send-picture.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
-import { ViewController } from 'ionic-angular';
+import { ViewController, AlertController } from 'ionic-angular';
 
 @Component({
     selector: 'page-send-picture',
     templateUrl: 'send-picture.html'
 })
 export class SendPicturePage {
+    public location: string = ''
 
-    constructor(private viewCtrl: ViewController) {
+    constructor(private viewCtrl: ViewController, private alertCtrl: AlertController) {
 
+    }
+
+    getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((data) => {
+                //always latitude first
+                this.location = data.coords.latitude + ', ' + data.coords.longitude
+            }, (err) => {
+                let alert = this.alertCtrl.create({
+                    title: 'Ops, algo de errado não está certo!',
+                    subTitle: 'Não foi possível encontrar a sua localização.',
+                    buttons: ['OK']
+                })
+                alert.present()
+            })
+        }
     }
 
     dismiss() {
