271dbd44ecafb6ef1d95476cdd367dec8a0c8fc1
Ral Oliver <raloliver.ws@gmail.com>
2017-12-08 16:04:43 -0200

    logoff OK

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,14 +1,28 @@
+import { LoginPage } from './../login/login';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { LoginPage } from '../login/login';
 
 @Component({
   selector: 'page-profile',
   templateUrl: 'profile.html'
 })
 export class ProfilePage {
+  //declare user variable
+  public user: string = ''
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private afAuth: AngularFireAuth) {
+    afAuth.authState.subscribe(user => {
+      if (user) {
+        this.user = user.email
+      }
+    })
   }
 
+  submit() {
+    //clear all stage logged
+    this.afAuth.auth.signOut()
+    this.navCtrl.setRoot(LoginPage)
+  }
 }
