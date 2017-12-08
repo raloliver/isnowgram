2f49f3d95c721548a407d21dc84e90645066c092
Ral Oliver <raloliver.ws@gmail.com>
2017-12-08 15:46:31 -0200

    login OK

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController, AlertController } from 'ionic-angular';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { AngularFireAuth } from 'angularfire2/auth';
 import { SignupPage } from './../signup/signup';
+import { HomePage } from '../home/home';
 
 
 @Component({
@@ -8,10 +11,51 @@ import { SignupPage } from './../signup/signup';
   templateUrl: 'login.html'
 })
 export class LoginPage {
+  public form: FormGroup
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(
+    private navCtrl: NavController,
+    private fb: FormBuilder,
+    private afAuth: AngularFireAuth,
+    private loadCtrl: LoadingController,
+    private alertCtrl: AlertController
+  ) {
+    this.form = this.fb.group({
+      email: ['', Validators.compose([
+        Validators.minLength(5),
+        Validators.maxLength(140),
+        Validators.required
+      ])],
+      password: ['', Validators.compose([
+        Validators.minLength(6),
+        Validators.maxLength(20),
+        Validators.required
+      ])]
+    })
   }
+
+  submit() {
+    let load = this.loadCtrl.create({ content: "Validando Dados..." })
+    load.present()
+
+    //promisse of signup
+    this.afAuth.auth.signInWithEmailAndPassword(
+        this.form.controls['email'].value,
+        this.form.controls['password'].value)
+        .then(() => {
+            load.dismiss()
+            this.navCtrl.setRoot(HomePage)
+        })
+        .catch(() => {
+            load.dismiss()
+            let alert = this.alertCtrl.create({
+                title: 'Ops, algo de errado não está certo!',
+                subTitle: 'Usuário ou senha estão incorretos.',
+                buttons: ['OK']
+            })
+            alert.present()
+        })
+}
 
   goToSignup() {
     this.navCtrl.setRoot(SignupPage)
