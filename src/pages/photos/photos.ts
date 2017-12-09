5aa20d8750873dbd335e25f0b2f232e6f48897e8
Ral Oliver <raloliver.ws@gmail.com>
2017-12-08 23:49:36 -0200

    add list of pictures on home

--- src/pages/photos/photos.ts
+++ src/pages/photos/photos.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { LoadingController } from 'ionic-angular';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+//import { ShowMapPage } from '../show-map/show-map';
 
 @Component({
     selector: 'page-photos',
     templateUrl: 'photos.html'
 })
 export class PhotosPage {
+    public pictures: any[] = []
 
-    constructor(public navCtrl: NavController) {
+    constructor(db: AngularFireDatabase, public loadCtrl: LoadingController) {
+        let load = this.loadCtrl.create({ content: "Atualizando Timeline..." })
+        load.present()
+
+        db.list<any>('/pictures').valueChanges().subscribe(pictures => {
+            this.pictures = pictures.reverse()
+            load.dismiss()
+        })
+        /*
+        showLocation(location) {
+            let modal = this.modalCtrl.create(ShowMapPage, { location: location });
+            modal.present();
+        }
+        */
 
     }
 
